94c19e100457caf80951d6c4ad00c09753a2407c
luna27N <nimalakaviya1980@gmail.com>
2026-07-09 21:50:45 +0530

    Add MoveZeroes - TwoPointers approach

---
+++ src/twopointers/MoveZeroes.java
@@ -0,0 +1,19 @@
+package twopointers;
+
+class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+
+        int k = 0;
+
+        for(int num : nums){
+
+            if(num != 0){
+                nums[k++] = num;
+            }
+        }
+
+        while(k < nums.length){
+            nums[k++] = 0;
+        }
+    }
+}
